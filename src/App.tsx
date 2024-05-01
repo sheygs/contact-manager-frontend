27e114473f210c5f23711348f7d14dc425346653
sheygs <macbookpro@sheygs-macbook.local>
2024-05-01 12:53:29 +0100

    chore: 🎉UI Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App(): JSX.Element {
+        return (
+                <div
+                        style={{
+                                border: '1px solid orange',
+                                background: 'orange',
+                                margin: '20% auto',
+                                width: '50%',
+                        }}
+                >
+                        <h1 style={{ textAlign: 'center' }}>Hux Assessment Frontend</h1>
+                </div>
+        );
+}
+
+export default App;
